81b1a2283cdc44c199abdd9ea22066fcae9260f3
edsolater <edsolater@qq.com>
2020-07-13 00:04:42 +0800

    加入快速排序的代码

---
+++ sort.js
@@ -0,0 +1,53 @@
+/**
+ * 快速排序
+ * @param {number[]} arr 
+ * @param {number} leftEdge 
+ * @param {number} rightEdge 
+ */
+function quickSort(arr, leftEdge = 0, rightEdge = arr.length - 1) {
+  if (leftEdge < rightEdge) {
+    const partitionIndex = partition(arr, leftEdge, rightEdge)
+    quickSort(arr, leftEdge, partitionIndex - 1)
+    quickSort(arr, partitionIndex + 1, rightEdge)
+  }
+  return arr
+}
+
+
+/**
+ * 分区操作
+ * @param {number[]} arr 
+ * @param {number} leftEdge 
+ * @param {number} rightEdge 
+ * @returns {number} 返回被选中的基准所处的位置
+ */
+function partition(
+  arr,
+  leftEdge, // 假定leftIndex就是被选中的基准的index
+  rightEdge
+) {
+  let p1 = leftEdge + 1 // p1 就是分界线
+  for (let p2 = leftEdge + 1; p2 <= rightEdge; p2++) {
+    if (arr[p2] < arr[leftEdge]) {
+      swap(arr, p2, p1)
+      p1++
+    }
+  }
+  swap(arr, leftEdge, p1 - 1)
+  return p1 - 1
+}
+
+/**
+ * 交换两个数组项的值
+ * @param {number} arr 
+ * @param {number} i 1号索引
+ * @param {number} j 2号索引
+ */
+function swap(arr, i, j) {
+  if (i !== j) {
+    const temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+  }
+}
+
--- treeTraversal.js
+++ treeTraversal.js
@@ -1,3 +1,9 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val
+  this.left = left === undefined ? null : left
+  this.right = right === undefined ? null : right
+}
+
 /**前序遍历 */
 function preorderTraversal(root) {
   if (!root) return []
@@ -10,6 +16,15 @@ function preorderTraversal(root) {
     if (node.left) stack.push(node.left)
   }
   return result
+}
+function preorderTraversal(root) {
+  return root
+    ? [
+        root.val,
+        ...preorderTraversal(root.left),
+        ...preorderTraversal(root.right),
+      ]
+    : []
 }
 // 后序遍历，相当于遍历顺序倒过来、先右后左的正序遍历
 function postorderTraversal(root) {
@@ -24,6 +39,15 @@ function postorderTraversal(root) {
   }
   result.reverse()
   return result
+}
+function postorderTraversal(root) {
+  return root
+    ? [
+        ...postorderTraversal(root.left),
+        ...postorderTraversal(root.right),
+        root.val,
+      ]
+    : []
 }
 
 /**中序遍历 */
@@ -43,7 +67,15 @@ function inorderTraversal(root) {
   }
   return result
 }
-
+function inorderTraversal(root) {
+  return root
+    ? [
+        ...inorderTraversal(root.left),
+        root.val,
+        ...inorderTraversal(root.right),
+      ]
+    : []
+}
 /**层序遍历 */
 function levelOrder(root) {
   if (!root) return []
@@ -130,37 +162,65 @@ function searchBST(root, val) {
 }
 
 /**插入搜索树节点 */
-function insertIntoBST(
-  root,
-  val,
-  { rootTree, parentNode, directionFromParent } = {
-    rootTree: root,
-  } //传入节点信息
-) {
+function insertIntoBST(root, val) {
   if (!root) {
-    if (parentNode) {
-      parentNode[directionFromParent] = new TreeNode(val)
-      return rootTree
-    } else {
-      return new TreeNode(val)
-    }
-  } else {
-    return val < root.val
-      ? insertIntoBST(root.left, val, {
-          rootTree,
-          parentNode: root,
-          directionFromParent: "left",
-        })
-      : insertIntoBST(root.right, val, {
-          rootTree,
-          parentNode: root,
-          directionFromParent: "right",
-        })
+    return new TreeNode(val)
   }
+  if (val < root.val) {
+    root.left = insertIntoBST(root.left, val)
+  } else if (val > root.val) {
+    root.right = insertIntoBST(root.right, val)
+  }
+  return root
 }
 
-function TreeNode(val, left, right) {
-  this.val = val === undefined ? 0 : val
-  this.left = left === undefined ? null : left
-  this.right = right === undefined ? null : right
+/**删除搜索树节点 */
+function deleteNode(root, key) {
+  if (!root) {
+    return null
+  }
+  if (key < root.val) {
+    root.left = deleteNode(root.left, key)
+  } else if (key > root.val) {
+    root.right = deleteNode(root.right, key)
+  } else if (root.val === key) {
+    if (!root.left && !root.right) {
+      //如果是叶子节点
+      root = null
+    } else if (root.right) {
+      // 1。让右边继任者等于当前值，再从右子树中递归删除继任者
+      root.val = successor(root).val
+      root.right = deleteNode(root.right, root.val)
+    } else if (root.left) {
+      //1. 从左子树中找到前任predecessor,为当前值，并从左子树删除
+      root.val = predecessor(root).val
+      root.left = deleteNode(root.left, root.val)
+    }
+  }
+  return root
+}
+
+/**
+ * 取后继节点（即比当前节点大的最小节点）
+ * 利用搜索树节点排列性质
+ * @param {TreeNode} root
+ */
+function successor(root) {
+  root = root.right
+  while (root.left) {
+    root = root.left
+  }
+  return root
+}
+/**
+ * 取前驱节点（即比当前节点小的最大节点）
+ * 利用搜索树节点排列性质
+ * @param {TreeNode} root
+ */
+function predecessor(root) {
+  root = root.left
+  while (root.right) {
+    root = root.right
+  }
+  return root
 }
