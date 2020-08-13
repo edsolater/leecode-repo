948fa09c056bdfe2d645bb2fbb1122daf6ac4d6b
edsolater <edsolater@qq.com>
2020-08-13 18:26:10 +0800

    新增 堆的操作 【补】

--- heapOperations.js
+++ heapOperations.js
@@ -1,41 +1,162 @@
-let getLeftNodeIndex = (index) => 2 * index + 1
-let getRightNodeIndex = (index) => 2 * index + 2
-let getParentNodeIndex = (index) => Math.ceil(index / 2) - 1
+/****************
+ * 个人觉得heap就是数组的一种视角,所以还是不要构建class了
+ */
 
 /**
- *  从头部维护堆。（只有一条维护路径）（认为头部节点是被外界替换的那一个的）
- * @param {number[]} heap
- * @param {'max' | 'min'} [heapType='max']
+ * 索引，一般是数组下标
+ * @typedef {number} Index
  */
-let maintainHeap = (heap = [], heapType = "max", _top = 0) => {
-  if (heap[_top] === undefined || heap[_top] === null) return heap //说明此处没有节点
-  let left = getLeftNodeIndex(_top)
-  let right = getRightNodeIndex(_top)
-  let extremum = (heapType === "max" ? Math.max : Math.min).apply(
-    null,
-    [heap[_top], heap[left], heap[right]].filter((n) => typeof n === "number")
-  )
 
-  if (heap[_top] === extremum) return heap
-  else if (heap[left] === extremum) {
-    ;[heap[left], heap[_top]] = [heap[_top], heap[left]]
-    return maintainHeap(heap, heapType, left)
-  } else {
-    ;[heap[right], heap[_top]] = [heap[_top], heap[right]]
-    return maintainHeap(heap, heapType, right)
+/**
+ * function-type: **mutation**
+ *
+ * 交换堆中的两个数据
+ * @param {number[]} heap 堆
+ * @param {Index} i1 要交换的索引
+ * @param {Index} i2 要交换的索引
+ */
+let swap = (heap, i1, i2) => {
+  ;[heap[i1], heap[i2]] = [heap[i2], heap[i1]]
+}
+
+/**
+ * function-type: **pure**
+ *
+ * 获取堆中下一个左节点的索引
+ * @param {Index} index 当前索引
+ */
+let getLeftNodeIndex = (index = 0) => (index >= 0 ? 2 * index + 1 : -1)
+
+/**
+ * function-type: **pure**
+ *
+ * 获取堆中下一个右节点的索引
+ * @param {Index} index 当前索引
+ */
+let getRightNodeIndex = (index = 0) => (index >= 0 ? 2 * index + 2 : -1)
+
+/**
+ * function-type: **pure**
+ *
+ * 获取堆中父节点的索引
+ * @param {Index} index 当前索引
+ */
+let getParentNodeIndex = (index = 0) =>
+  index >= 0 ? Math.ceil(index / 2) - 1 : -1
+
+/**
+ * function-type: **mutation**
+ *
+ * 插入一个新节点
+ * @param {number[]} heap 目标堆
+ * @param {'max'|'min'} heapType 指示是大/小堆
+ * @param {number} newItem 要插入的数据
+ */
+let insert = (heap = [], heapType = "max", newItem) => {
+  heap.push(newItem)
+  let cur = heap.length - 1
+  while (cur > -1) {
+    let parent = getParentNodeIndex(cur)
+    if (parent === -1) return
+    if (
+      heapType === "max" ? heap[parent] >= heap[cur] : heap[cur] <= heap[parent]
+    ) {
+      break
+    } else {
+      swap(heap, cur, parent)
+      cur = parent
+    }
   }
 }
 
 /**
- *  插入式建堆
- * @param {number[]} candidates
- * @param {'max' | 'min'} [heapType='max']
+ * function-type: **mutation**
+ *
+ * 弹出堆顶元素
+ *
+ * - 与堆底元素互换
+ * - 删除堆底元素（原堆顶元素）
+ * - 从堆顶元素开始（原堆底元素），维护堆
+ * @param {number[]} heap 目标堆
+ * @param {'max'|'min'} [heapType='max'] 指示是大/小堆
+ * @param {number} newItem 要插入的数据
+ */
+let popHeapTop = (heap = [], heapType = "max") => {
+  swap(heap, 0, heap.length - 1)
+
+  heap.pop()
+
+  let cur = 0
+  while (true) {
+    let left = getLeftNodeIndex(cur)
+    let right = getRightNodeIndex(cur)
+    if (right === -1 && left === -1) return
+    let extremum = (heapType === "max" ? Math.max : Math.min).apply(
+      null,
+      [heap[cur], heap[left], heap[right]].filter((n) => typeof n === "number")
+    )
+    if (heap[cur] === extremum) {
+      break
+    } else if (heap[left] === extremum) {
+      swap(heap, cur, left)
+      cur = left
+    } else {
+      swap(heap, cur, right)
+      cur = right
+    }
+  }
+}
+
+/**
+ * function-type: **pure**
+ *
+ * 插入式建堆
+ * @param {number[]} candidates 数组
+ * @param {'max' | 'min'} [heapType='max'] 指示是大/小堆
+ * @returns {number[]} 堆
  */
 let createHeap = (candidates = [], heapType = "max") => {
   let resultHeap = []
   for (let item of candidates) {
-    resultHeap.unshift(item)
-    maintainHeap(resultHeap, heapType)
+    insert(resultHeap, heapType, item)
   }
   return resultHeap
+}
+
+/**
+ * function-type: **pure**
+ *
+ * 获取最小的几个数
+ * @param {number[]} arr 目标数组
+ * @param {number} k 需要多少极值
+ * @return {number[]}  极值数组
+ */
+let getLeastNumbers = function (arr, k) {
+  let myHeap = createHeap(arr.slice(0, k))
+  for (let n of arr.slice(k)) {
+    if (n < myHeap[0]) {
+      popHeapTop(myHeap)
+      insert(myHeap, "max", n)
+    }
+  }
+  return myHeap
+}
+
+/**
+ * function-type: **pure**
+ *
+ * 获取较大的几个数
+ * @param {number[]} arr 目标数组
+ * @param {number} k 需要多少极值
+ * @return {number[]}  极值数组
+ */
+let getLeastNumbers = function (arr, k) {
+  let myHeap = createHeap(arr.slice(0, k))
+  for (let n of arr.slice(k)) {
+    if (n < myHeap[0]) {
+      popHeapTop(myHeap)
+      insert(myHeap, "min", n)
+    }
+  }
+  return myHeap
 }
